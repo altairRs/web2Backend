1a516a61755c9309beebe9f99195d47a2c6902d2
Aweary69 <globaleliteforever@gmail.com>
2025-01-09 17:36:47 +0500

    git commit -m "Added .env file and updated db.js; removed old seceretkey.env"

--- db.js
+++ db.js
@@ -1,20 +1,13 @@
 const mongoose = require('mongoose');
 
-
-const MONGO_URI = 'mongodb+srv://zeniszns:Mambetism@cluster0.8wgxh.mongodb.net/';
-
-
 const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI, {
-      
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error.message);
     process.exit(1); // Exit process with failure
   }
 };
-
 
 module.exports = connectDB;
